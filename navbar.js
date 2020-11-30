30c55c351ec38375241024beb3f59340dae23c63
marcinfilipowicz <73066958+marcinfilipowicz@users.noreply.github.com>
2020-11-30 17:45:44 +0100

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,3 @@
+let nav = '<ul><ul class="nav"><li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li><li class="nav-item"><a class="nav-link" href="kontakt.html">Kontakt</a></li></ul></ul>'
+let a=document.querySelector('.navbar')
+a.innerHTML = nav;
